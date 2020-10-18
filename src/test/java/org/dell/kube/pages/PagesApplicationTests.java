e24d94b668ef4af81aed4a005f259982f54d68eb
Rupchand_Dewangan <Rupchand_Dewangan@dell.com>
2020-10-18 13:04:09 +0530

    First commit

---
+++ src/test/java/org/dell/kube/pages/PagesApplicationTests.java
@@ -0,0 +1,15 @@
+package org.dell.kube.pages;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
